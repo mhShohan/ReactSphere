0d04f5bb8232aca8b19134146d7f8289fb3dce2b
mhShohan <mehdihasanshohan25@gmail.com>
2025-03-25 22:59:04 +0600

    nested not found

--- src/app/(main)/layout.tsx
+++ src/app/(main)/layout.tsx
@@ -7,7 +7,7 @@ const MainLayout = ({ children }: { children: React.ReactNode }) => {
     <SidebarProvider>
       <AppSidebar />
       <SidebarInset>
-        <header className='flex h-16 shrink-0 items-center gap-2 transition-[width,height] ease-linear group-has-[[data-collapsible=icon]]/sidebar-wrapper:h-12'>
+        <header className='flex h-16 border-b border-foreground/10 items-center gap-2 transition-[width,height] ease-linear group-has-[[data-collapsible=icon]]/sidebar-wrapper:h-12'>
           <div className='flex w-full items-center justify-between gap-2 px-4'>
             <SidebarTrigger className='-ml-1' />
             <ModeToggle />
@@ -15,9 +15,7 @@ const MainLayout = ({ children }: { children: React.ReactNode }) => {
         </header>
 
         {/* Children */}
-        <main className='p-4'>
-          <div className='p-4'>{children}</div>
-        </main>
+        <main className='py-4 px-6'>{children}</main>
       </SidebarInset>
     </SidebarProvider>
   );
