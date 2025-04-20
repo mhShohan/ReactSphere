8e04292631b4562f713bcf6a53f746d517a85686
mhShohan <mehdihasanshohan25@gmail.com>
2025-04-20 10:12:15 +0600

    add avatar images

---
+++ src/utils/images.ts
@@ -0,0 +1,12 @@
+export const images = [
+  { url: '/assets/images/avatar/boy.png', category: 'avatar' },
+  { url: '/assets/images/avatar/profile.png', category: 'avatar' },
+  { url: '/assets/images/avatar/gamer.png', category: 'avatar' },
+  { url: '/assets/images/avatar/human.png', category: 'avatar' },
+  { url: '/assets/images/avatar/man.png', category: 'avatar' },
+  { url: '/assets/images/avatar/man1.png', category: 'avatar' },
+  { url: '/assets/images/avatar/man2.png', category: 'avatar' },
+  { url: '/assets/images/avatar/man3.png', category: 'avatar' },
+  { url: '/assets/images/avatar/woman.png', category: 'avatar' },
+  { url: '/assets/images/avatar/woman1.png', category: 'avatar' },
+];
