3d7f68d271bc94bb1e35e89928c26b1c11748c07
mhShohan <mehdihasanshohan25@gmail.com>
2025-05-24 23:42:46 +0600

    pie

---
+++ src/components/charts/PeiChart.tsx
@@ -0,0 +1,49 @@
+'use client';
+
+import ChartDataGenerator from '@/lib/ChartDataGenerator';
+import { IChartDataset } from '@/types';
+import {
+  ArcElement,
+  BarElement,
+  CategoryScale,
+  Chart as ChartJS,
+  Legend,
+  LinearScale,
+  LineElement,
+  PointElement,
+  RadialLinearScale,
+  Title,
+  Tooltip,
+  type ChartData,
+} from 'chart.js';
+import { Bar } from 'react-chartjs-2';
+import { verticalChartOptions } from './chartOptions';
+
+ChartJS.register(
+  CategoryScale,
+  LinearScale,
+  PointElement,
+  LineElement,
+  BarElement,
+  ArcElement,
+  RadialLinearScale,
+  Title,
+  Tooltip,
+  Legend
+);
+
+interface PieChartProps {
+  data: IChartDataset[];
+}
+
+const PeiChart = ({ data }: PieChartProps) => {
+  const chartData: ChartData<'bar'> = ChartDataGenerator.bar(data);
+
+  return (
+    <div className='h-[500px] w-full border border-gray-200 rounded-xl py-5 bg-white'>
+      <Bar data={chartData} options={verticalChartOptions} />;
+    </div>
+  );
+};
+
+export default PeiChart;
